8befe1d4ce8d408ac085e83cba26904e34c51161
budiljak <ben@udiljak.de>
2014-07-30 15:11:02 +0200

    Test-Seeds wie Development-Seeds

--- db/seeds/test.rb
+++ db/seeds/test.rb
@@ -9,6 +9,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 require 'date'
+require 'time'
 
 objekts = Objekt.create([{bezeichner:"Objekt 1"}, {bezeichner:"Objekt 2"}])
 benutzers = Benutzer.create([{login:'admin', passwort:'1234', vorname:"Der große", nachname:"Admin", typ:VERWALTER}, {login:'kriecher', passwort:'1234', vorname:"Herbert", nachname:"Kriecher", typ:MITARBEITER, objekts:objekts}, {login:'huber', passwort:'12345', vorname:"Werner", nachname:"Huber", typ:MITARBEITER, objekts:[objekts.first]}, {login:'kunde', passwort:'123456', vorname:"Konrad", nachname:"Kunde", typ:KUNDE, objekts:[objekts.last]}])
@@ -35,14 +36,15 @@ while tag < DateTime.now do
     {objekt: objekts[0], benutzer: benutzers[1], datum: tag, uhrzeit_beginn: (tag + 18.hours).strftime("%F"), uhrzeit_ende: tag + 26.hours}
   ])
   schichts.each do |s|
+    zeitBeginn = Time.parse(s.uhrzeit_beginn)
     Rapport.create([
-      {schicht: s, beschreibung: 'beschr1', ort:'ort1', uhrzeit: (s.uhrzeit_beginn + 2.hours).strftime("%F"), massnahmen: 'massn1', position: 1}, 
-      {schicht: s, beschreibung: 'beschr2', ort:'ort2', uhrzeit: (s.uhrzeit_beginn + 4.hours).strftime("%F"), massnahmen: 'massn2', position: 2}, 
-      {schicht: s, beschreibung: 'beschr3', ort:'ort3', uhrzeit: (s.uhrzeit_beginn + 6.hours).strftime("%F"), massnahmen: 'massn3', position: 3}
+      {schicht: s, beschreibung: 'beschr1', ort:'ort1', uhrzeit: (zeitBeginn + 2.hours).strftime("%F"), massnahmen: 'massn1', position: 1}, 
+      {schicht: s, beschreibung: 'beschr2', ort:'ort2', uhrzeit: (zeitBeginn + 4.hours).strftime("%F"), massnahmen: 'massn2', position: 2}, 
+      {schicht: s, beschreibung: 'beschr3', ort:'ort3', uhrzeit: (zeitBeginn + 6.hours).strftime("%F"), massnahmen: 'massn3', position: 3}
     ])
     wb = WachbuchEintrag.create({schicht: s, besonderheiten: 'bes', schaeden: 'schä', schluessel_bemerkung: 'schlüBem'})
-    Kontrollanruf.create({wachbuch_eintrag: wb, uhrzeit: s.uhrzeit_beginn + 2.hours, bemerkung: 'bem', objekt: 'obj'})
-    Kontrollgang.create({wachbuch_eintrag: wb, uhrzeit: s.uhrzeit_beginn + 4.hours, bemerkung: 'bem'})
+    Kontrollanruf.create({wachbuch_eintrag: wb, uhrzeit: zeitBeginn + 2.hours, bemerkung: 'bem', objekt: 'obj'})
+    Kontrollgang.create({wachbuch_eintrag: wb, uhrzeit: zeitBeginn + 4.hours, bemerkung: 'bem'})
   end
   tag = tag + 1.days
 end
