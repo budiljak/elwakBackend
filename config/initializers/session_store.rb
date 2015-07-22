e79c8ac9471e5e8334a379790176b2a4de9d6ae1
Benjamin Udiljak <ben@udiljak.de>
2015-07-22 14:44:12 +0200

    Kleine Bugfixes (Tippfeher; Session-Timeout; Passwoerter nicht speichern bei 'Passwort aendern'; ...)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-ElwakBackend::Application.config.session_store :cookie_store, key: '_elwakBackend_session', expire_after: 24.hours
+ElwakBackend::Application.config.session_store :cookie_store, {key: '_elwakBackend_session', expire_after: 1.hours}
